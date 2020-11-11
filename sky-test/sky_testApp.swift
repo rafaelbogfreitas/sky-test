c311a90e65def597e405acc0a2e491e63f9b34f1
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-11-11 20:35:33 -0300

    Initial Commit

---
+++ sky-test/sky_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  sky_testApp.swift
+//  sky-test
+//
+//  Created by Rafael Freitas on 11/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct sky_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
