271001db5f57c7186347a7be9e88ed6852f3c2f1
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-11-13 00:31:38 -0300

    Create a Detail view

--- sky-test/screens/detail/DetailView.swift
+++ sky-test/screens/detail/DetailView.swift
@@ -8,13 +8,46 @@
 import SwiftUI
 
 struct DetailView: View {
+    
+    var movie: Movie
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 20) {
+                    MovieCover(url: movie.cover_url)
+                    Text(movie.title)
+                        .fontWeight(.bold)
+                        .font(.title)
+                    Text(movie.overview)
+                        .font(.body)
+            
+                    HStack(
+                        alignment: .center,
+                        spacing: 20
+                    ) {
+                        
+                        Group {
+                            
+                            HStack {
+                                Image(systemName: "clock")
+                                Text(movie.duration)
+                            }
+                            
+                            HStack {
+                                Image(systemName: "calendar")
+                                Text(movie.release_year)
+                            }
+                        }
+                        .font(.body)
+                        
+                    }
+                }
+                .padding()
+            }
+        }
+        .foregroundColor(.black)
     }
 }
 
-struct DetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        DetailView()
-    }
-}
+
