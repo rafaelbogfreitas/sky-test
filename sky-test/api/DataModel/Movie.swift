5f0c53ab9147db2d612a792501ed6966ff964e55
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-11-13 00:30:28 -0300

    Create Codable data model to parse json form api

--- sky-test/api/DataModel/Movie.swift
+++ sky-test/api/DataModel/Movie.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct Movie: Codable, Identifiable {
+    var title: String
+    var overview: String
+    var duration: String
+    var release_year: String
+    var cover_url: String
+    var backdrops_url: [String]
+    var id: String
+}
+
