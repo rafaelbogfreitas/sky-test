b139a2518586f61d526ea785e52b9b1af90921d4
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-11-13 00:40:43 -0300

    Create reusable ApiManager file

--- sky-test/api/ApiManager.swift
+++ sky-test/api/ApiManager.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+struct ApiManager {
+    
+    public func performRequest<T: Codable>(
+        url: String,
+        type: T.Type,
+        onComplete: @escaping (T) -> Void
+    ) -> Void {
+        if let url = URL(string: url) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                
+                if error == nil {
+                    let decoder = JSONDecoder()
+                    if let safeData = data {
+                        do {
+                            let data = try decoder.decode(type, from: safeData)
+                            onComplete(data)
+                        } catch {
+                            print(error)
+                        }
+                    }
+                }
+            }
+            
+            task.resume()
+        }
+    }
+}
