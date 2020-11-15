e350040425829cf30eae2d2c1ce30bd75f323078
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-11-15 19:53:26 -0300

    Implement Favorites Screen

--- sky-test/screens/favorites/FavoritesView.swift
+++ sky-test/screens/favorites/FavoritesView.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct FavoritesView: View {
+    
+    @State var movies: [MovieEntity]
+    
+    var favorites: [MovieEntity] {
+        return movies.filter {
+           return UserDefaults.standard.bool(forKey: $0.id)
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(favorites) { movie in
+            NavigationLink(destination: DetailView(movie: movie)) {
+                HStack (alignment: .bottom){
+                    MovieCover(url: movie.cover_url)
+                    VStack(alignment: .leading) {
+                        Text(movie.title)
+                            .font(.body)
+                            .fontWeight(.bold)
+                        TimeAndYear(duration: movie.duration, year: movie.release_year)
+                    }
+                    .frame(width: 100)
+                }
+            }
+            
+        }
+        .navigationTitle("Favoritos ♥️")
     }
 }
 
-struct FavoritesView_Previews: PreviewProvider {
-    static var previews: some View {
-        FavoritesView()
-    }
-}
+
