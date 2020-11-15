0bd9ac41793f7648fc46209d64ecbc310681d17c
Rafael Freitas <rafaelborges1985@hotmail.com>
2020-11-15 19:54:00 -0300

    Use MovieEntity to convert data

--- sky-test/screens/main/MainScreenModel.swift
+++ sky-test/screens/main/MainScreenModel.swift
@@ -9,22 +9,23 @@ import SwiftUI
 
 class MainScreenModel: ObservableObject {
     
-   @Published var movies = [Movie]()
+    @Published var movies = [MovieEntity]()
     
     var url = "https://sky-exercise.herokuapp.com/api/Movies"
     
     func fetchData() {
+        
         if let url = URL(string: url) {
             let session = URLSession(configuration: .default)
             let task = session.dataTask(with: url) { (data, response, error) in
-                
+
                 if error == nil {
                     let decoder = JSONDecoder()
                     if let safeData = data {
                         do {
                             let data = try decoder.decode([Movie].self, from: safeData)
                             DispatchQueue.main.async {
-                                self.movies = data
+                                self.movies = MovieEntity.mapper(movies: data)
                             }
                         } catch {
                             print(error)
@@ -32,9 +33,10 @@ class MainScreenModel: ObservableObject {
                     }
                 }
             }
-            
+
             task.resume()
         }
     }
+    
 }
 
